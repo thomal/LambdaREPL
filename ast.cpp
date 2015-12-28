b425e58b91703f82fea554e41dedab096764c9ad
Luke Thomas <luke@luke-thomas.net>
2015-12-28 07:08:26 +0000

    Implemented α-reduction.

--- ast.cpp
+++ ast.cpp
@@ -1,10 +1,32 @@
-#include <string>
-#include <stdlib.h>
 #include "ast.hpp"
-
+#include <string>
+#include <stdlib.h> //exit(1)
+#include <string.h> //strdup
 #include <iostream>
+#include <sstream>  //ostringstream
+#include <stdint.h> //uint64_t
 using namespace std;
 
+//////// ID GENERATION /////////////////////////////////////////////////////////
+typedef uint64_t vid_t;
+vid_t globallyUniqueID = 0; //Maximum number of variables
+
+std::string newID () {
+    id_t oldid = globallyUniqueID;
+    id_t newid = ++globallyUniqueID;
+    cout << "newID(): " << oldid << "->" << newid << "\n";
+    if (newid > oldid) {
+        std::ostringstream oss;
+        oss << newid;
+        return oss.str();
+    } else {
+        cout << "Variable limit of 2^64 bound variables exceeded.\n";
+        exit(1); //Overflow, 2^64 variable limit exceeded
+    }
+}
+////////////////////////////////////////////////////////////////////////////////
+
+//////// VARIABLE //////////////////////////////////////////////////////////////
 Variable::~Variable () {
     free(name);
 }
@@ -13,6 +35,24 @@ std::string Variable::toString() {
     return std::string(name);
 }
 
+void Variable::alphaReduce(std::string oldname, std::string newname) {
+    if (strcmp(name, oldname.c_str())==0) {
+        free(name);
+        name = strdup(newname.c_str());
+        hasUniqueName = true;
+    }
+}
+
+void Variable::betaReduce() {
+    //TODO
+}
+
+bool Variable::isClosed() {
+    return hasUniqueName;
+}
+////////////////////////////////////////////////////////////////////////////////
+
+//////// ABSTRACTION ///////////////////////////////////////////////////////////
 Abstraction::~Abstraction () {
     delete v;
     delete n;
@@ -22,6 +62,31 @@ std::string Abstraction::toString() {
     return "(λ" + v->toString() + "." + n->toString() + ")";
 }
 
+void Abstraction::alphaReduce(std::string oldname, std::string newname) {
+    //If we aren't shadowing oldname alpha-reduce rhs
+    if (strcmp(v->name,oldname.c_str())!=0)
+        n->alphaReduce(oldname, newname);
+    
+    //Now alpha reduce the variable we bind to a unique name.
+    //If we've already done so in a previous call then skip this.
+    if (!v->hasUniqueName) {
+        string newNameForOurArg = newID();
+        string oldNameForOurArg = std::string(v->name);
+        v->alphaReduce(oldNameForOurArg, newNameForOurArg);
+        n->alphaReduce(oldNameForOurArg, newNameForOurArg);
+    }
+}
+
+void Abstraction::betaReduce() {
+    //TODO
+}
+
+bool Abstraction::isClosed() {
+    return n->isClosed();
+}
+////////////////////////////////////////////////////////////////////////////////
+
+//////// APPLICATION ///////////////////////////////////////////////////////////
 Application::~Application () {
     delete lhs;
     delete rhs;
@@ -30,3 +95,17 @@ Application::~Application () {
 std::string Application::toString() {
     return "(" + lhs->toString() + " " + rhs->toString() + ")";
 }
+
+void Application::alphaReduce(std::string oldname, std::string newname) {
+    lhs->alphaReduce(oldname, newname);
+    rhs->alphaReduce(oldname, newname);
+}
+
+void Application::betaReduce() {
+    //TODO
+}
+
+bool Application::isClosed() {
+    return lhs->isClosed() && rhs->isClosed();
+}
+////////////////////////////////////////////////////////////////////////////////
--- ast.hpp
+++ ast.hpp
@@ -6,15 +6,24 @@ class ASTNode {
   public:
     virtual ~ASTNode () {};
     virtual std::string toString() = 0;
+    virtual void alphaReduce(std::string oldname = "", std::string newname = "") = 0;
+    virtual void betaReduce() = 0;
+    
+    //NB: This function only works following an alpha-reduction
+    virtual bool isClosed() = 0;
 };
 
 class Variable : public ASTNode {
   public:
-    Variable  (char* _name) : name(_name) {}
+    Variable  (char* _name) : name(_name), hasUniqueName(false) {}
     virtual ~Variable ();
     virtual std::string toString();
+    virtual void alphaReduce(std::string oldname = "", std::string newname = "");
+    virtual void betaReduce();
+    virtual bool isClosed();
     
     char* name;
+    bool hasUniqueName;
 };
 
 class Abstraction : public ASTNode {
@@ -22,6 +31,9 @@ class Abstraction : public ASTNode {
     Abstraction (Variable* _v, ASTNode* _n) : v(_v), n(_n) {}
     virtual ~Abstraction ();
     virtual std::string toString();
+    virtual void alphaReduce(std::string oldname = "", std::string newname = "");
+    virtual void betaReduce();
+    virtual bool isClosed();
     
     Variable* v;
     ASTNode* n;
@@ -32,6 +44,9 @@ class Application : public ASTNode {
     Application (ASTNode* _lhs, ASTNode* _rhs) : lhs(_lhs), rhs(_rhs) {}
     virtual ~Application ();
     virtual std::string toString();
+    virtual void alphaReduce(std::string oldname = "", std::string newname = "");
+    virtual void betaReduce();
+    virtual bool isClosed();
     
     ASTNode* lhs;
     ASTNode* rhs;
